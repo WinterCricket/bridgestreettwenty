a8ff4a682a998bf7bdfc5f5007c3e7809520a220
WinterCricket <hodhodhod2003@gmail.com>
2020-07-10 01:06:48 -0500

    building out some of the interactive blog page

---
+++ homepage.php
@@ -0,0 +1,107 @@
+
+<?php get_header(); ?>
+
+
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+    <div class="page-banner__content container t-center c-white">
+      <h1 class="headline headline--large">Welcome!</h1>
+      <h2 class="headline headline--medium">We think you&rsquo;ll suffer self-doubt here.</h2>
+      <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>structures</strong> you&rsquo;re interested in?</h3>
+      <a href="#" class="btn btn--large btn--blue">Master Your Form</a>
+    </div>
+  </div>
+
+  <div class="full-width-split group">
+    <div class="full-width-split__one">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+        
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month">Mar</span>
+            <span class="event-summary__day">25</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+            <p>Bring poems you&rsquo;ve written in crayon and deposit them at 100 Mile House this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month">Apr</span>
+            <span class="event-summary__day">02</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Change-Your-Diaper Socially Distant Party</a></h5>
+            <p>Live music, dead music, dry-and-wet tunes, tickling sessions, and more you can find at our change-your-diaper day. <a href="#" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+        
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+
+      </div>
+    </div>
+    <div class="full-width-split__two">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+
+        <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="#">
+            <span class="event-summary__month">Jan</span>
+            <span class="event-summary__day">20</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Are an Elite Writing Program</a></h5>
+            <p>Directors of Iowa Writers Workshop Voted Bridge Steet #1. <a href="#" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+        <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="#">
+            <span class="event-summary__month">Feb</span>
+            <span class="event-summary__day">04</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Writing Mentors in the International Spotlight</a></h5>
+            <p>Six of our Writing Mentors Major national and international literary awards. <a href="#" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+        
+        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="hero-slider">
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Free Transformations</h2>
+        <p class="t-center">Smoking jackets are supplied to all new students.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Eye Apples</h2>
+        <p class="t-center">Our poetry program recommends staring at apples.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Free Food</h2>
+        <p class="t-center">Bridge Street offers secular communion with buttered bread.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer();
+ ?>
