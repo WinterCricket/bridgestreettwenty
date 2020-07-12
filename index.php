6c4d8587706383f6ead2786c5701b47ade18a8ce
WinterCricket <hodhodhod2003@gmail.com>
2020-07-11 22:54:26 -0500

    add links and create blog single page functions

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ get_header();
       <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
       <div class="metabox">
-        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in category</p>
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
 
       <div class="generic-content">
@@ -33,6 +33,7 @@ get_header();
 
     <?php
   } 
+    echo paginate_links();
   ?>
 
 </div>
