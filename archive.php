029c1349eec569f4da6065e757c3c8a14c9695b1
WinterCricket <hodhodhod2003@gmail.com>
2020-07-15 18:41:17 -0500

    add some archive func to blog

--- archive.php
+++ archive.php
@@ -6,7 +6,13 @@ get_header();
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"> Welcome to the Street Blog!</h1>
+    <h1 class="page-banner__title"> <?php if(is_category()){
+      single_cat_title(); echo " Archive";
+    } 
+    if(is_author()){
+      echo 'Posts from '; the_author();
+    }
+    ?></h1>
     <div class="page-banner__intro">
       <p>Keep up with the writing world!</p>
     </div>
