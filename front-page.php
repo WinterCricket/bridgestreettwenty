60b43e30a3a7fa057b053ac8402da763020862fe
WinterCricket <hodhodhod2003@gmail.com>
2020-07-15 22:27:13 -0500

    join archive categories to frontpage lists

--- front-page.php
+++ front-page.php
@@ -46,27 +46,31 @@
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+        <?php 
+          $homepagePosts = new WP_Query(array(
+            'posts_per_page' => 2
+          ));
 
+          while($homepagePosts ->have_posts()){
+            $homepagePosts->the_post(); ?>
+            
+          
+
+        
         <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Jan</span>
-            <span class="event-summary__day">20</span>  
+          <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month"><?php the_time('M'); ?></span>
+            <span class="event-summary__day"><?php the_time('d'); ?></span>  
           </a>
           <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Are an Elite Writing Program</a></h5>
-            <p>Directors of Iowa Writers Workshop Voted Bridge Steet #1. <a href="#" class="nu gray">Read more</a></p>
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
           </div>
         </div>
-        <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Feb</span>
-            <span class="event-summary__day">04</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Writing Mentors in the International Spotlight</a></h5>
-            <p>Six of our Writing Mentors Major national and international literary awards. <a href="#" class="nu gray">Read more</a></p>
-          </div>
-        </div>
+
+        <?php
+          } wp_reset_postdata();
+         ?>
         
         <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
